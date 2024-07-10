eacddddc80ef659a297665ba0492a65a64cf3fb1
MOHTISHAMKHAN <155059864+MOHTISHAMKHAN@users.noreply.github.com>
2024-07-10 15:46:28 -0700

    Add files via upload
    sign up then login then dashboard then back to sign up

---
+++ login.js
@@ -0,0 +1,33 @@
+
+let formFeild = document.querySelectorAll("form input");
+
+const [loginEmail, loginPassword] = formFeild;
+
+
+const data = JSON.parse(localStorage.getItem("userData"));
+
+let loader = document.getElementById('loader');
+const login = () => {
+    event.preventDefault();
+    const { signUpEmail, signupPassword } = data;
+
+    if ((signUpEmail === loginEmail.value) &&
+        signupPassword === loginPassword.value) {
+        
+        loader.classList.add('loader');
+        setTimeout(() => {
+            window.location.href = '../Dashboard/dashboard.html'
+            loader.classList.remove('loader');
+        }, 2000)
+
+    }
+    else {
+        alert("Invalid Credentils")
+    }
+
+
+
+     console.log(signUpEmail, signupPassword);
+     console.log(loginEmail.value, loginPassword.value);
+}
+
